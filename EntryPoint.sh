a8ee6ab2941bf4ede92aab8a179b8d709a9d08cf
Mainuddin Talukdar <mtalukdar@vantage.com>
2023-12-18 23:59:45 -0600

    added frontend running also

--- EntryPoint.sh
+++ EntryPoint.sh
@@ -13,9 +13,6 @@ cd backend
 #Add two following line in /etc/profile
 echo -e 'export JAVA_HOME=/usr/lib/jvm/jre-21\nexport PATH=$JAVA_HOME/bin:$PATH' | sudo tee -a /etc/profile
 source /etc/profile
-#export JAVA_HOME=/usr/lib/jvm/jre-21
-#export PATH=$JAVA_HOME/bin:$PATH
-# sudo cp -f ../profile /etc/
 
 ./mvnw clean install
 java -jar target/auction-app-0.0.1-SNAPSHOT.jar > java-spring.log 2>&1 &
@@ -24,17 +21,18 @@ java -jar target/auction-app-0.0.1-SNAPSHOT.jar > java-spring.log 2>&1 &
 
 
 #running fronend server
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-# export NVM_DIR="$HOME/.nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-# . ~/.nvm/nvm.sh
-# nvm install 16
-# yum install -y nginx
+cd ..
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+. ~/.nvm/nvm.sh
+nvm install 16
+yum install -y nginx
 
-# cd ../frontend
-# npm install
-# npm run build
-# sudo cp -f ../nginx.conf /etc/nginx/
-# service nginx start
-# chkconfig nginx on
+cd frontend
+npm install
+npm run build
+sudo cp -f ../nginx.conf /etc/nginx/
+service nginx start
+chkconfig nginx on
